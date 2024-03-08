2c3f1f9a6bee6cbe16f078dd71d5b99680eef6c0
iliadeleon <iliadeleon@gmail.com>
2024-03-08 17:26:35 -0500

    add mobile side bar and logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavDesktop from "@/app/components/NavDesktop";
 import Footer from "@/app/components/Footer";
+import NavMobile from "@/app/components/NavMobile";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,8 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className}`}>
-        <div className="px-24">
+        <div className="px-6 py-8 md:px-24 md:px-0">
           <NavDesktop />
+          <NavMobile />
           {children}
         </div>
         <Footer />
