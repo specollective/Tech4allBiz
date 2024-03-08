60934c28bb9c49c7e17e8570addacaa1899130ae
Yoline <92892101+YolineB@users.noreply.github.com>
2024-03-08 13:14:04 -0800

    Correct footer size, position, and fix hydration error

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,11 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} px-24`}>
-        <NavDesktop />
-        {children}
+      <body className={`${inter.className}`}>
+        <div className="px-24">
+          <NavDesktop />
+          {children}
+        </div>
+        <Footer />
       </body>
-      <Footer />
     </html>
   );
 }
