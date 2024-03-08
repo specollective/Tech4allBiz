b551a3b14c3dded201ed0cf62df622d9b71a2c2a
iliadeleon <iliadeleon@gmail.com>
2024-03-08 13:02:09 -0500

    add nav desktop

---
+++ app/components/NavDesktop.tsx
@@ -0,0 +1,7 @@
+export default function NavDesktop() {
+  return (
+    <nav className="bg-pink-300">
+      <div>tech4allbiz</div>
+    </nav>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import styles from "@/app/page.module.css";
 import Footer from "@/app/components/Footer";
 import { getLessonsFormatterAndSlug } from "@/app/components/mdx-content";
 import Link from "next/link";
+import NavDesktop from "@/app/components/NavDesktop";
 
 export default async function Home() {
   const lessons = await getLessonsFormatterAndSlug();
   return (
     <main className={styles.main}>
+      <NavDesktop />
       <h1>Lessons</h1>
       <ul>
         {lessons.map((lesson) => (
