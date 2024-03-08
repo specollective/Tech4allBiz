e7c7608dca022602fb80cc51ed1fc638af99c5f3
iliadeleon <iliadeleon@gmail.com>
2024-03-08 13:31:12 -0500

    align navbar container

--- app/components/NavDesktop.tsx
+++ app/components/NavDesktop.tsx
@@ -1,7 +1,12 @@
 export default function NavDesktop() {
   return (
-    <nav className="bg-pink-300">
+    <nav className="bg-pink-300 flex flex-row justify-between items-center w-full h-[100px]">
       <div>tech4allbiz</div>
+      <div className="flex flex-row gap-10">
+        <div>Home</div>
+        <div>Lessons</div>
+        <div>Free Online Resources</div>
+      </div>
     </nav>
   );
 }
