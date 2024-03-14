f45dba55f172f5648e0d03fb09f9a7696a21f64c
Yoline <92892101+YolineB@users.noreply.github.com>
2024-03-14 11:34:23 -0700

    Add Online resources page

---
+++ app/online-resources/page.tsx
@@ -0,0 +1,9 @@
+export default function OnlineResourcesPage() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen bg-gray-100">
+        <div className="text-xl font-bold text-gray-900">Hello!</div>
+      </div>
+    </>
+  );
+}
