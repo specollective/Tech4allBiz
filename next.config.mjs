1801f81b1fca09bf21cb5b56fd8fd689d4c23566
Joe Torreggiani <joetorreggiani@gmail.com>
2024-03-15 18:17:52 -0400

    Only export in production.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  distDir: "build",
-  output: "export",
-};
+const nextConfig = {};
+
+if (process.env.NODE_ENV === "production") {
+  nextConfig["distDir"] = "build";
+  nextConfig["output"] = "export";
+}
 
 export default nextConfig;
