2e8b4b1de0bb132626ce0e9a285e0f0c3c4ade41
iliadeleon <iliadeleon@gmail.com>
2024-03-21 16:12:07 -0400

    correct lesson hero style, add author

--- app/components/lesson/LessonHero.tsx
+++ app/components/lesson/LessonHero.tsx
@@ -3,10 +3,10 @@ import { FrontmatterProps } from "@/app/constants/types";
 const styles = {
   container: "w-full flex flex-col",
   headerRow: "flex flex-row w-full",
-  title: "md:basis-2/5 text-3xl bold",
-  contentRow: "flex flex-col md:flex-row justify-between w-full",
-  description: "md:basis-2/5 mt-4 mb-4",
-  statsContainer: "flex flex-col mt-auto",
+  title: "md:basis-3/5 text-3xl bold",
+  contentRow: "flex flex-col md:flex-row justify-between  w-full",
+  description: "md:basis-3/5 mt-4 mb-4",
+  statsContainer: "flex flex-col align-top mt-4",
   statsItem: "",
 };
 
@@ -20,6 +20,7 @@ export default function LessonHero({ frontmatter }: FrontmatterProps) {
       <div className={styles.contentRow}>
         <div className={styles.description}>{frontmatter.description}</div>
         <div className={styles.statsContainer}>
+          <div className={styles.statsItem}>{`By: ${frontmatter.author}`}</div>
           <div
             className={styles.statsItem}
           >{`Number of Lesson Steps: ${frontmatter.steps}`}</div>
