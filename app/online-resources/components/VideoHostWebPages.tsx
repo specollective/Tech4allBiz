403bbec92d65fff0c2429ac51964df1d108588c9
iliadeleon <iliadeleon@gmail.com>
2024-04-03 16:43:40 -0400

    add host name style

--- app/online-resources/components/VideoHostWebPages.tsx
+++ app/online-resources/components/VideoHostWebPages.tsx
@@ -1,9 +1,13 @@
 import { hostPages } from "@/app/constants/hostPages";
 import { Title3 } from "@/app/ui/Titles";
 
+const styles = {
+  container: "flex flex-col gap-5 py-10",
+  name: "underline hover:text-[#0E4873]",
+};
 export default function VideoHostWebPage() {
   return (
-    <section className="flex flex-col gap-5 py-10">
+    <section className={styles.container}>
       <Title3>Video Host Web Pages</Title3>
       {hostPages.map((host, index) => (
         <a
@@ -11,7 +15,7 @@ export default function VideoHostWebPage() {
           href={host.website}
           target="_blank"
           rel="noopener noreferrer"
-          className=""
+          className={styles.name}
         >
           {host.name}
         </a>
