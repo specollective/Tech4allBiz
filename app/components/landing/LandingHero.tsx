2818621097596a05760e1584f55e242f6f396717
Yoline <92892101+YolineB@users.noreply.github.com>
2024-04-02 18:01:32 -0700

    Content Update Apr 2 (#22)
    * Update landing page hero
    
    * Update online resources
    
    * Update host page links
    
    * Update lessons

--- app/components/landing/LandingHero.tsx
+++ app/components/landing/LandingHero.tsx
@@ -1,6 +1,6 @@
 import Button from "@/app/ui/Buttons";
 import { BodyRegular } from "@/app/ui/Texts";
-import { Title1 } from "@/app/ui/Titles";
+import { Title3 } from "@/app/ui/Titles";
 
 const styles = {
   container: "flex justify-between flex-col md:flex-row w-full md:py-5",
@@ -16,10 +16,14 @@ const LandingHero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.leftColumn}>
-        <Title1 className={styles.header}>Tech4allBiz</Title1>
+        <Title3>
+          {" "}
+          Welcome by Vicky Tamaru, buildJUSTLY & Tech4AllBiz Founder{" "}
+        </Title3>
         <BodyRegular className={styles.paragraph}>
-          Lorem ipsum dolor sit amet consectetur. Pellentesque a quis mauris
-          tempus tortor aliquet arcu eu. A mauris urna duis euismod.
+          Welcome to Tech4all.biz, a website to help small business owners just
+          like you build skills and confidence step by step by showing how other
+          small business owners integrate technology into their business.
         </BodyRegular>
         <div className={styles.buttonContainer}>
           <Button href="/#lessons" variant="lessons" text="View Lessons" />
@@ -32,7 +36,7 @@ const LandingHero = () => {
       </div>
       <div className={styles.rightColumn}>
         <iframe
-          src="https://www.youtube.com/embed/lE6RYpe9IT0?si=73rcqCe79hdufTfT"
+          src="https://www.youtube.com/embed/zHKNJj_Avfc?si=hClSZB5cqWZZS6B6"
           title="YouTube video"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
--- app/constants/onlineResources.ts
+++ app/constants/onlineResources.ts
@@ -3,24 +3,30 @@ export const onlineResources = [
     title: "Small Business Digital Alliance Free Digital Tools",
     description:
       " More than 300 free digital tools for small business owners and entrepreneurs to accelerate your online marketing strategies, learn to utilize data to understand customers, reach new markets, recruit and retain hiring talent, and more.",
-    link: "https://www.google.com/",
+    link: "https://www.smallbusinessdigitalalliance.com/",
   },
   {
     title: "Digital Skills Library • Digital Skills Library",
     description:
       "The Digital Skills Library is an open repository of free learning resources designed to help all adult learners develop the digital skills needed to achieve their personal, civic, educational, and career goals.",
-    link: "https://www.google.com/",
+    link: "https://digitalskillslibrary.org/",
   },
   {
     title: "AT&T - DigitalLearn",
     description:
       "Are you new to computers or mobile devices, haven't used them for a while or just need a refresher? Learn the basics and build confidence using technology.",
-    link: "https://www.google.com/",
+    link: "https://att.digitallearn.org/",
   },
   {
     title: "Small Business Digital Maturity Compass",
     description:
-      "Are you new to computers or mobile devices, haven't used them for a while or just need a refresher? Learn the basics and build confidence using technology.",
+      "Discover in less than 5 minutes how your tech adoption stands with the AI for Social Progress Digital Maturity Compass and gain tailored actionable insights.",
     link: "https://www.google.com/",
+  },
+  {
+    title: "WSMA - Evergreen Biz Link Navigator",
+    description:
+      "Evergreen BizLink is a supportive platform that connects entrepreneurs to a network of Washington State resource partners to foster innovation, growth, and prosperity. Powered by the Washington State Microenterprise Association (WSMA), in collaboration with the WA State Department of Commerce, the founding partners of Evergreen BizLink include the Small Business Administration (SBA), Minority Business Development Agency (MBDA), Washington Small Business Development Center (SBDC), Washington Main Street and Washington Economic Development Association (WEDA).",
+    link: "https://evergreenbizlink.com/",
   },
 ];
